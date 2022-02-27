62db8c64773ad96e694f786cfc0d8e87074b6e52
mierealaura <mierealaura@gmail.com>
2022-02-27 19:34:48 +0200

    b

---
+++ pageController.js
@@ -0,0 +1,42 @@
+var input = document.getElementById("myInput");
+        var fullScreen = false;
+        /* Get the documentElement (<html>) to display the page in fullscreen */
+        var elem = document.documentElement;
+
+        window.addEventListener('keydown', (event) =>{
+            if(event.key === "f"){
+                pushFullscreenButton()
+            }
+        });
+
+        function pushFullscreenButton(){
+            if(!fullScreen){
+                openFullscreen()
+            }else{
+                closeFullscreen()
+            }
+        }
+        
+        /* View in fullscreen */
+        function openFullscreen() {
+          if (elem.requestFullscreen) {
+            elem.requestFullscreen();
+          } else if (elem.webkitRequestFullscreen) { /* Safari */
+            elem.webkitRequestFullscreen();
+          } else if (elem.msRequestFullscreen) { /* IE11 */
+            elem.msRequestFullscreen();
+          }
+          fullScreen = true;
+        }
+        
+        /* Close fullscreen */
+        function closeFullscreen() {
+          if (document.exitFullscreen) {
+            document.exitFullscreen();
+          } else if (document.webkitExitFullscreen) { /* Safari */
+            document.webkitExitFullscreen();
+          } else if (document.msExitFullscreen) { /* IE11 */
+            document.msExitFullscreen();
+          }
+          fullScreen = false;
+        }
